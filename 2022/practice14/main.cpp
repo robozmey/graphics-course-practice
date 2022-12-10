a80ec95f34bf7abeffec6a51264933e708925fd5
Vladimir Rachkin <vova@kpnn.ru>
2022-12-10 16:56:12 +0300

    Add practice14 solution

--- 2022/practice14/main.cpp
+++ 2022/practice14/main.cpp
@@ -56,13 +56,14 @@ uniform mat4 projection;
 layout (location = 0) in vec3 in_position;
 layout (location = 1) in vec3 in_normal;
 layout (location = 2) in vec2 in_texcoord;
+layout (location = 3) in vec3 in_instance_position;
 
 out vec3 normal;
 out vec2 texcoord;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(in_position, 1.0);
+    gl_Position = projection * view * model * vec4(in_position + in_instance_position, 1.0);
     normal = mat3(model) * in_normal;
     texcoord = in_texcoord;
 }
@@ -140,13 +141,13 @@ int main() try
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 8);
     SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-    SDL_Window * window = SDL_CreateWindow("Graphics course practice 11",
+    SDL_Window * window = SDL_CreateWindow("Graphics course practice 14",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         800, 600,
@@ -190,6 +191,8 @@ int main() try
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, input_model.buffer.size(), input_model.buffer.data(), GL_STATIC_DRAW);
 
+
+    std::vector<GLuint> offsets_vbos;
     std::vector<GLuint> vaos;
     for (int i = 0; i < input_model.meshes.size(); ++i)
     {
@@ -210,6 +213,14 @@ int main() try
         setup_attribute(1, input_model.meshes[i].normal);
         setup_attribute(2, input_model.meshes[i].texcoord);
 
+        GLuint offsets_vbo;
+        glGenBuffers(1, &offsets_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, offsets_vbo);
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glVertexAttribDivisor(3, 1);
+
+        offsets_vbos.push_back(offsets_vbo);
         vaos.push_back(vao);
     }
 
@@ -244,9 +255,31 @@ int main() try
 
     bool paused = false;
 
+    std::vector<GLuint> timer_queries;
+    std::vector<bool> is_timer_queries_free;
+    int free_timer_queries = 0;
+
     bool running = true;
     while (running)
     {
+        GLuint current_timer_query;
+        bool is_free_timer_query_found = false;
+        for (int timer_query_index = 0; timer_query_index < timer_queries.size(); timer_query_index++) {
+            if (is_timer_queries_free[timer_query_index]) {
+                is_timer_queries_free[timer_query_index] = false;
+                is_free_timer_query_found = true;
+                current_timer_query = timer_queries[timer_query_index];
+                free_timer_queries--;
+            }
+        }
+        if (!is_free_timer_query_found) {
+            GLuint new_timer_query;
+            glGenQueries(1, &new_timer_query);
+            timer_queries.push_back(new_timer_query);
+            is_timer_queries_free.push_back(false);
+            current_timer_query = new_timer_query;
+        }
+
         for (SDL_Event event; SDL_PollEvent(&event);) switch (event.type)
         {
         case SDL_QUIT:
@@ -306,6 +339,8 @@ int main() try
         camera_position += camera_move_forward * glm::vec3(-std::sin(camera_rotation), 0.f, std::cos(camera_rotation));
         camera_position += camera_move_sideways * glm::vec3(std::cos(camera_rotation), 0.f, std::sin(camera_rotation));
 
+        glBeginQuery(GL_TIME_ELAPSED, current_timer_query);
+
         glClearColor(0.8f, 0.8f, 1.f, 0.f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -328,6 +363,37 @@ int main() try
 
         glm::vec3 light_direction = glm::normalize(glm::vec3(1.f, 2.f, 3.f));
 
+        frustum frustum_object = frustum(projection * view);
+
+//        glBindBuffer(GL_ARRAY_BUFFER, offsets_vbo);
+        std::vector<glm::vec3> frustum_offsets;
+        int scale = 16;
+        for (int i = -scale; i < scale; i++) {
+            for (int j = -scale; j < scale; j++) {
+                glm::vec3 current_offset = {i, 0, j};
+                aabb current_aabb = aabb(input_model.meshes[0].min + current_offset, input_model.meshes[0].max + current_offset);
+                if (intersect(frustum_object, current_aabb))
+                    frustum_offsets.push_back(current_offset);
+            }
+        }
+
+        std::vector<std::vector<glm::vec3>> lod_offsets(offsets_vbos.size());
+        for (int offset_index = 0; offset_index < frustum_offsets.size(); offset_index++) {
+            float distance = glm::distance(camera_position, frustum_offsets[offset_index]);
+            int lod_level = distance / 3;
+            if (lod_level >= lod_offsets.size())
+                lod_level = lod_offsets.size()-1;
+            lod_offsets[lod_level].push_back(frustum_offsets[offset_index]);
+        }
+
+        for (int lod_level = 0; lod_level < lod_offsets.size(); lod_level++) {
+            std::cout << lod_level << ": " << lod_offsets[lod_level].size() << ", ";
+
+            glBindBuffer(GL_ARRAY_BUFFER, offsets_vbos[lod_level]);
+            glBufferData(GL_ARRAY_BUFFER, lod_offsets[lod_level].size() * sizeof(lod_offsets[lod_level][0]), lod_offsets[lod_level].data(), GL_DYNAMIC_DRAW);
+        }
+        std::cout << std::endl;
+
         glUseProgram(program);
         glUniformMatrix4fv(model_location, 1, GL_FALSE, reinterpret_cast<float *>(&model));
         glUniformMatrix4fv(view_location, 1, GL_FALSE, reinterpret_cast<float *>(&view));
@@ -336,10 +402,27 @@ int main() try
 
         glBindTexture(GL_TEXTURE_2D, texture);
 
-        {
-            auto const & mesh = input_model.meshes[0];
-            glBindVertexArray(vaos[0]);
-            glDrawElements(GL_TRIANGLES, mesh.indices.count, mesh.indices.type, reinterpret_cast<void *>(mesh.indices.view.offset));
+        for (int lod_level = 0; lod_level < lod_offsets.size(); lod_level++) {
+            auto const &mesh = input_model.meshes[lod_level];
+            glBindVertexArray(vaos[lod_level]);
+            glDrawElementsInstanced(GL_TRIANGLES,
+                                    mesh.indices.count,
+                                    mesh.indices.type,
+                                    reinterpret_cast<void *>(mesh.indices.view.offset), lod_offsets[lod_level].size());
+        }
+
+        glEndQuery(GL_TIME_ELAPSED);
+
+        for (int timer_query_index = 0; timer_query_index < timer_queries.size(); timer_query_index++) {
+            GLint is_timer_query_ready;
+            glGetQueryObjectiv(timer_queries[timer_query_index], GL_QUERY_RESULT_AVAILABLE, &is_timer_query_ready);
+            if (is_timer_query_ready) {
+                free_timer_queries++;
+                is_timer_queries_free[timer_query_index] = true;
+                GLint result;
+                glGetQueryObjectiv(timer_queries[timer_query_index], GL_QUERY_RESULT, &result);
+                std::cout << "fd: " << double(result) / 1e9 << ", tqs: " << timer_queries.size() - free_timer_queries << ", fps: "  << 1e9 / double(result) << std::endl;
+            }
         }
 
         SDL_GL_SwapWindow(window);
